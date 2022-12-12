dafd8b510ade481731851e6d6905300070e887f1
ShawnCodingShit <113091553+ShawnCodingShit@users.noreply.github.com>
2022-12-12 21:25:22 +0900

    Update SWGithubGroup.java

--- SWGithubGroup.java
+++ SWGithubGroup.java
@@ -1,82 +1,92 @@
-class BankAccount {
-	
-	private int balance;
-	private int TRANSFER_FEE = 1;
-	private boolean frozen; 
-	
-	public BankAccount(int initial_amount) {
-		balance = initial_amount;
-        frozen = false;
-	}
-	
-	public boolean deposit(int amount) {
-        if (frozen) {
-            return false;
-        }
-        balance = balance + amount;
-        return true;
-	}
-	  
-	public boolean withdraw(int amount) { 
-        if (frozen) {
-            return false;
-        }
-        if (balance < amount) {
-            return false;
-        }
-        balance = balance - amount;
-        return true;
-	}
+import java.util.Scanner;
 
-	public boolean transfer(int amount, BankAccount other) {
-	
-        if (frozen) {
-            return false;
-        }
-        if (balance < amount + 1) {
-            return false;
-        }
-        if (other.frozen) {
-            return false;
-        }
-        balance = balance - amount - TRANSFER_FEE;
-        other.balance = other.balance + amount;
-        return true;
-
-
-	}
-	
-	public void freeze() {
-        frozen = true;
-	} 
-	
-	public boolean isFrozen() {
-            return frozen;
-
-	}
-	
-	public int getBalance() {
-            return balance;
-	}	
+public class Bankaccount {
+    public static void main(String[] args) {
+        Bank bank = new Bank();
+        bank.createAccount();
+        bank.login();
+        bank.deposit();
+        bank.withdraw();
+        bank.transfer();
+    }
 }
 
-public class SWGithubGroup {
+class Bank {
+    private String account;
+    private String password;
+    private double balance;
+    private String name;
+    private String id;
+    private String phone;
+    private String address;
 
-	public static void main(String[] args) {
-		
-		BankAccount account1 = new BankAccount(10000);
-		BankAccount account2 = new BankAccount(10);
-		System.out.println(account1.transfer(10000, account2));  
-		System.out.println(account1.transfer(95, account2));  
-		System.out.println("account 1: " + account1.getBalance()); 
-		System.out.println("account 2: " + account2.getBalance()); 
-		System.out.println(account1.transfer(58, account2)); 
-		System.out.println("account 1: " + account1.getBalance()); 
-		System.out.println("account 2: " + account2.getBalance()); 
-        account2.freeze();
-        System.out.println(account1.transfer(69, account2));
-        System.out.println("account 1: " + account1.getBalance());
-        System.out.println("account 2: " + account2.getBalance());
-	}
+    public void createAccount() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("请输入账户：");
+        account = input.next();
+        System.out.println("请输入密码：");
+        password = input.next();
+        System.out.println("请输入姓名：");
+        name = input.next();
+        System.out.println("请输入身份证号：");
+        id = input.next();
+        System.out.println("请输入电话号码：");
+        phone = input.next();
+        System.out.println("请输入地址：");
+        address = input.next();
+        System.out.println("创建账户成功！");
+    }
 
+    public void login() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("请输入账户：");
+        String account = input.next();
+        System.out.println("请输入密码：");
+        String password = input.next();
+        if (account.equals(this.account) && password.equals(this.password)) {
+            System.out.println("登录成功！");
+        } else {
+            System.out.println("登录失败！");
+        }
+    }
+
+    public void deposit() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("请输入存款金额：");
+        double money = input.nextDouble();
+        balance += money;
+        System.out.println("存款成功！");
+    }
+
+    public void withdraw() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("请输入取款金额：");
+        double money = input.nextDouble();
+        if (money > balance) {
+            try {
+                throw new BankException("余额不足！");
+            } catch (BankException e) {
+                e.printStackTrace();
+            }
+        } else {
+            balance -= money;
+            System.out.println("取款成功！");
+        }
+    }
+
+    public void transfer() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("请输入转账金额：");
+        double money = input.nextDouble();
+        if (money > balance) {
+            try {
+                throw new BankException("余额不足！");
+            } catch (BankException e) {
+                e.printStackTrace();
+            }
+        } else {
+            balance -= money;
+            System.out.println("转账成功！");
+        }
+    }
 }
